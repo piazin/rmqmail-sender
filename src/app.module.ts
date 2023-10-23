6972d7cdb0e6bfc1231745eb86deeb13b2d406ce
Lucas <ls4803326@gmail.com>
2023-10-22 23:51:16 -0300

    :wrench: chore: added an environment variable for connecting to CloudAMQP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EmailQueueModule } from './email-queue/email-queue.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmailQueueModule],
+  imports: [
+    EmailQueueModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/email-queue/email-queue.module.ts
+++ src/email-queue/email-queue.module.ts
@@ -2,22 +2,27 @@ import { Module } from '@nestjs/common';
 import { EmailQueueService } from './email-queue.service';
 import { EmailQueueController } from './email-queue.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
-        name: "EMAIL_QUEUE_SERVICE",
-        transport: Transport.RMQ,
-        options: {
-          urls: ["amqp://localhost:5672"],
-          queue: "purchase_confirmation_email_queue",
-          queueOptions: {
-            durable: false
-          }
-        }
-      }
-    ])
+        name: 'EMAIL_QUEUE_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            noAck: true,
+            queue: 'purchase_confirmation_email_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [EmailQueueController],
   providers: [EmailQueueService],
